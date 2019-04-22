2161c35ee77e419949d76f224322e244c7f453fb
CaroFG <carolina.ferreira131@gmail.com>
2019-04-22 22:52:03 +0200

    commentaires ajoutés

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -16,15 +16,24 @@ class Gossip
 	end
 
 	def self.all
+		#On crée un array vide pour stocker tous les potins
 	  all_gossips = []
+	  #On selectionne le fichier et on parcourt chaque ligne 
 	  CSV.read("./db/gossip.csv").each do |csv_line|
+	  #On stocke dans notre array chaque ligne et pour chacune
+	  #les deux premières colonnes correspondantes à l'autheur 
+	  #et au contenu du potin et on crée une instance de gossip avec
 	    all_gossips << Gossip.new(csv_line[0], csv_line[1])
 	  end
 	  return all_gossips
 	end
 
 	def self.find (id)
+		# On va chercher le fichier CSV
+		# On le stocke dans une variable sous forme d'array
+		# il contient un array par ligne
 		db_content = CSV.read("./db/gossip.csv")
+		# On selectionne le commentaire par son index qui va correspondre à l'id de la page
 		db_content[id]
 	end
 
